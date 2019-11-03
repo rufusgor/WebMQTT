f50e5db44c63d8684b136656d51e3eac4121d0cc
Jozsef Zolyomi <jozsef.zolyomi@hotmail.hu>
2019-11-03 06:48:01 +0100

    .

--- js/paho-mqtt.js
+++ js/paho-mqtt.js
@@ -2017,7 +2017,7 @@ function onMessageArrived(message) {
 							var port = connectOptions.ports[i];
 
 							var ipv6 = (host.indexOf(":") !== -1);
-							uri = "wss://"+(ipv6?"["+host+"]":host)+":"+port+path;
+							uri = "ws://"+(ipv6?"["+host+"]":host)+":"+port+path;
 							connectOptions.uris.push(uri);
 						}
 					} else {
